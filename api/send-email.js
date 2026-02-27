2c085c41cabbdd0420561de093ec5e2bcb47bab9
jinwoo8732-crypto <jinwoo8732@gmail.com>
2026-02-27 14:12:52 +0900

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -2,22 +2,28 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req,res){
 
-const transporter=nodemailer.createTransport({
-host:"smtp.gmail.com",
-port:587,
-secure:false,
-auth:{
-user:process.env.SMTP_USER,
-pass:process.env.SMTP_PASS
-}
-});
+  try{
 
-await transporter.sendMail({
-from:process.env.SMTP_USER,
-to:"받을메일주소",
-subject:"새 견적 접수",
-text:`${req.body.items}\n총액:${req.body.total}`
-});
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth:{
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
 
-res.status(200).json({success:true});
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: process.env.SMTP_USER,
+      subject: "새 견적 접수",
+      text: `내용:\n${req.body.items}\n\n총액:${req.body.total}`
+    });
+
+    res.status(200).json({success:true});
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({error:error.message});
+  }
+
 }
